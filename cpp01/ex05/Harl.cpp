c914c28806cd011f33c01d03a95add717c989929
notnotniche <nicolas@littletalesapp.com>
2023-12-06 14:23:37 +0100

    almost finished

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,5 @@
+#include "Harl.hpp"
+
+void Harl::debug(){
+    
+}
---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nklingsh <nklingsh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 15:04:20 by nklingsh          #+#    #+#             */
+/*   Updated: 2023/11/22 19:46:29 by nklingsh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	std::string filename;
+	std::string outfilename;
+	std::ifstream infile;
+	std::ofstream newfile;
+	std::string  line;
+
+	if (argc != 4)
+		return (std::cout << "Invalid number of arguments " << std::endl , 1);
+	
+	filename = argv[1];
+	outfilename = filename + ".replace";
+	infile.open(filename.c_str());
+	if (infile.is_open() == false)
+		return (std::cout << "Open infile failed" << std::endl , 1);
+	newfile.open(outfilename.c_str());
+	if (newfile.is_open() == false)
+		return (std::cout << "Open / Creation of new file failed" << std::endl , 1);
+	std::getline(infile, line, '\0');
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+	for (size_t i = 0; i < line.length(); i++)
+	{
+		if (i == line.find(s1))
+		{
+			line.erase(i, s1.length());
+			line.insert(i, s2);
+			i = i + s2.length();
+		}
+	}
+	newfile << line;
+	infile.close();
+	newfile.close();
+	return (0);
+}
