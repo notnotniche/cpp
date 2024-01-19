40c1707a810e752c98677ae2f71a21b17b0b55fe
notnotniche <nicolas@littletalesapp.com>
2024-01-19 12:16:42 +0100

    push

---
+++ cpp02/ex02/Fixed.cpp
@@ -0,0 +1,108 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    _fixedPoint = 0;
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int Param)
+{
+    std::cout << "Int Constructor Called" << std::endl;
+    this->_fixedPoint = (Param << _FractionalBits);
+}
+
+Fixed::Fixed(const float Param)
+{
+    std::cout << "Float Constructor Called" << std::endl;
+    this->_fixedPoint = int(roundf(Param * (1 << _FractionalBits)));
+}
+
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor Called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    _fixedPoint = other._fixedPoint;
+    std::cout << "Copy constructor was called" << std::endl;
+}
+
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+    _fixedPoint = rhs._fixedPoint;
+    std::cout << "Copy assignement operator Called" << std::endl;
+    return *this;
+}
+
+bool Fixed::operator>(const Fixed &rhs)
+{
+    
+}
+
+Fixed Fixed::operator+(const Fixed &rhs)
+{
+    Fixed ret;
+
+    ret.setRawBits(this->_fixedPoint + rhs._fixedPoint);
+    return ret;
+}
+
+Fixed Fixed::operator-(const Fixed &rhs)
+{
+    Fixed ret;
+
+    ret.setRawBits(this->_fixedPoint - rhs._fixedPoint);
+    return ret;
+}
+
+Fixed Fixed::operator/(const Fixed &rhs)
+{
+    Fixed ret;
+    if (rhs._fixedPoint == 0)
+    {  
+        std::cout << "Dividing by 0 is illegal you will go to jail now" << std::endl;
+        ret.setRawBits(this->_fixedPoint);
+    }
+    else
+        ret.setRawBits((this->_fixedPoint * (1 << _FractionalBits)) / rhs._fixedPoint);
+    return ret;
+}
+
+Fixed Fixed::operator*(const Fixed &rhs)
+{
+    Fixed ret;
+
+    ret.setRawBits(this->_fixedPoint * rhs._fixedPoint >> _FractionalBits);
+    return ret;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "GetRawBits function called" << std::endl;
+    return this->_fixedPoint;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_fixedPoint = raw;
+}
+
+float Fixed::toFloat(void) const
+{
+    return float(this->_fixedPoint) / (1 << _FractionalBits);
+}
+
+int Fixed::toInt(void) const
+{
+    return this->_fixedPoint >> _FractionalBits;
+}
+
+std::ostream& operator<<(std::ostream& o, const Fixed& fixed)
+{
+	o << fixed.toFloat();
+	return o;
+}
---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,40 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <string.h>
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed &other);
+        Fixed(const int Param);
+        Fixed(const float Param);
+
+        Fixed &operator=(const Fixed &rhs);
+        Fixed operator+(const Fixed &rhs);
+        Fixed operator-(const Fixed &rhs);
+        Fixed operator/(const Fixed &rhs);
+        Fixed operator*(const Fixed &rhs);
+        bool operator>(const Fixed &rhs);
+        bool operator>>(const Fixed &rhs);
+        bool operator<(const Fixed &rhs);
+        bool operator<<(const Fixed &rhs);
+        bool operator==(const Fixed &rhs);
+        bool operator!=(const Fixed &rhs);
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
+    private:
+        int _fixedPoint;
+        static int const _FractionalBits = 8;
+};
+
+std::ostream& operator<<(std::ostream& o, const Fixed& fixed);
+
+#endif
